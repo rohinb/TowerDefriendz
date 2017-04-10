d27e1f787e5f049d6d562e406e9f8a2655214e9f
Sahand Edrisian <sahandedrisian@Sahands-iMac.local>
2017-04-10 16:43:00 -0700

    Initial Commit

---
+++ Tower Defriendz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tower Defriendz
+//
+//  Created by Sahand Edrisian on 4/10/17.
+//  Copyright © 2017 fiveever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
